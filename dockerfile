c52e87a51bc10f67329428f779a17c80b76b555c
desarda7 <desarda.mece@fti.edu.al>
2024-01-23 16:00:35 +0100

    last-commit

--- dockerfile
+++ dockerfile
@@ -5,3 +5,5 @@ WORKDIR /usr/app
 ARG JAR_FILE=target/RandomNumberGenerator-0.0.1-SNAPSHOT.jar
 ADD ${JAR_FILE} generator.jar
 ENTRYPOINT ["java","-jar","generator.jar"]
+# Configure logging to write to a local file in append mode
+RUN ln -sf /dev/stdout /app/RandomNumberGenerator.log
--- src/main/java/RandomNumberGenerator/Controller.java
+++ src/main/java/RandomNumberGenerator/Controller.java
@@ -1,27 +1,60 @@
+// package RandomNumberGenerator;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import java.util.concurrent.ThreadLocalRandom;
+// import java.util.logging.FileHandler;
+// import java.util.logging.Logger;
+// import java.util.logging.SimpleFormatter;
+// import org.springframework.boot.SpringApplication;
+
+// @RestController
+// public class Controller {
+
+
+// @GetMapping("/generate")
+// public ResponseEntity<Integer> generateRandomNumber() {
+//     int randomNumber = ThreadLocalRandom.current().nextInt(3000, 444101);
+//     return ResponseEntity.ok(randomNumber);
+// }
+// }
+
 package RandomNumberGenerator;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
-import org.springframework.boot.SpringApplication;
 
 @RestController
 public class Controller {
 
-// 	@RequestMapping("/gjenero")
-// 	public int index() {
-// //		String style = "\"color:white;font-size:10rem;position: absolute; top: 30%;left: 50%;-moz-transform: translateX(-50%) translateY(-50%);-webkit-transform: translateX(-50%) translateY(-50%);transform: translateX(-50%) translateY(-50%);\"";
-// //		return "<!doctypehtml><html><head><title>RandomNumber</title></head><body style=\"background:black\"><h1 style="
-// //				+ style + ">" + new Random().nextInt(1000001) + "</h1></body></html>";
+    private static final Logger logger = Logger.getLogger(Controller.class.getName());
 
-// 		return new Random().nextInt(1000001);
-// 	}
-@GetMapping("/generate")
-public ResponseEntity<Integer> generateRandomNumber() {
-    int randomNumber = ThreadLocalRandom.current().nextInt(3000, 444101);
-    return ResponseEntity.ok(randomNumber);
+    static {
+        try {
+            // Set up file handler for logging (append mode)
+            FileHandler fileHandler = new FileHandler("randomNumberGenerator.log", true);
+            fileHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/generate")
+    public ResponseEntity<Integer> generateRandomNumber() {
+        int randomNumber = ThreadLocalRandom.current().nextInt(3000, 444101);
+
+        // Log the generated random number
+        logger.info("Generated random number: " + randomNumber);
+
+        return ResponseEntity.ok(randomNumber);
+    }
 }
-}
+
